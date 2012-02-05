20aab07fef785d28d8dc4945e0d3906e2d264d46
Dimitar Dimitrov <mykob.11@gmail.com>
2012-02-05 23:22:19 +0200

    d-wallet-android first commit

---
+++ src/com/ddimitroff/projects/dwallet/android/DWalletActivity.java
@@ -0,0 +1,13 @@
+package com.ddimitroff.projects.dwallet.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DWalletActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
