eeaf45d61f02dbb7a1b29a66331142a6deb63995
Dimitar Dimitrov <mykob.11@gmail.com>
2012-02-18 23:00:07 +0200

    url for cloud added

--- src/com/ddimitroff/projects/dwallet/android/DWalletAndroidUtils.java
+++ src/com/ddimitroff/projects/dwallet/android/DWalletAndroidUtils.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class DWalletAndroidUtils {
 
-	public static final String DWALLET_PROPERTY_SERVER_URL = "http://77.70.35.220/d-wallet-web/rest";
+	public static final String DWALLET_PROPERTY_SERVER_URL = "http://dwallet.cloudfoundry.com/rest";
 	public static final String DWALLET_PROPERTY_API_KEY = "testAPIkey1";
 
 	public static final String convertStreamToString(InputStream is) {
