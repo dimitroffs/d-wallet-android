005298dd327e6e7289434ce28fe51236c6289f4a
ddimitroff <mykob.11@gmail.com>
2012-09-12 23:55:37 +0300

    added dwallet tab activity

---
+++ src/com/ddimitroff/projects/dwallet/android/DWalletTabActivity.java
@@ -0,0 +1,36 @@
+package com.ddimitroff.projects.dwallet.android;
+
+import android.app.TabActivity;
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public abstract class DWalletTabActivity extends TabActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    // set orientation for all activities
+    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+  }
+
+  protected abstract boolean validate();
+
+  public boolean isOnline() {
+    ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+    NetworkInfo netInfo = cm.getActiveNetworkInfo();
+    if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+      return true;
+    }
+    return false;
+  }
+
+  public void makeToast(int resId) {
+    Toast.makeText(this, resId, Toast.LENGTH_SHORT).show();
+  }
+  
+}
