c16e73abeacc6c5799c3b58142a9156657a51550
ddimitroff <mykob.11@gmail.com>
2013-02-02 13:14:45 +0200

    Initial commit to Bitbucket

---
+++ gen/com/ddimitroff/projects/dwallet/android/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.ddimitroff.projects.dwallet.android;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
